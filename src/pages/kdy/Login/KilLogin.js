e89c0f8dc8c67c1635da673361621967c6d16124
sanha <kgpaper@naver.com>
2021-11-18 11:22:16 +0900

    Add: 로그인 페이지 및 로그인 유효성 검사 구현 - 로그인 페이지 레이아웃 구현 - useState를 이용한 ID/PW 인풋의 상태값 조작 가능 - ID/PW의 특정 조건을 충족하면 로그인버튼 활성화

--- src/pages/kdy/Login/KilLogin.js
+++ src/pages/kdy/Login/KilLogin.js
@@ -1,7 +1,90 @@
-import React from 'react';
+import { React, useState, useRef } from 'react';
+import './KilLogin.scss';
 
 const KilLogin = () => {
-  return <div>hi</div>;
+  const [userInfo, setUserInfo] = useState({ userId: '', userPw: '' });
+  const [checkId, setCheckId] = useState({ value: false });
+  const [checkPw, setCheckPw] = useState({ value: false });
+  const loginButton = useRef();
+
+  const { userId, userPw } = userInfo;
+
+  const handleUserInput = e => {
+    const { name, value } = e.target;
+    setUserInfo({
+      ...userInfo,
+      [name]: value,
+    });
+
+    idValid();
+    pwValid();
+  };
+
+  const idValid = () => {
+    const regex = /[0-9a-zA-z-_.]*@[0-9a-zA-z-_.]/g;
+
+    if (userInfo.userId.match(regex) != null) {
+      setCheckId(true);
+    } else {
+      setCheckId(false);
+    }
+
+    loginButton.current.disabled = !(checkId && checkPw);
+  };
+
+  const pwValid = () => {
+    if (userInfo.userPw.length <= 4) {
+      setCheckPw(false);
+    } else {
+      setCheckPw(true);
+    }
+
+    loginButton.current.disabled = !(checkId && checkPw);
+  };
+
+  return (
+    <>
+      <body>
+        <main className="login-main">
+          <div className="login-wrapper">
+            <h1 className="h1back">westagram</h1>
+            <div className="form-container">
+              <form id="login">
+                <div className="input-wrapper">
+                  <input
+                    name="userId"
+                    type="text"
+                    placeholder="아이디"
+                    id="id"
+                    onChange={handleUserInput}
+                  />
+                </div>
+                <div className="input-wrapper">
+                  <input
+                    name="userPw"
+                    type="password"
+                    placeholder="비밀번호"
+                    id="pw"
+                    onChange={handleUserInput}
+                  />
+                </div>
+                <div className="input-wrapper">
+                  <button id="login-button" disabled ref={loginButton}>
+                    로그인
+                  </button>
+                </div>
+                <div className="input-wrapper pw-remind">
+                  <a href="">비밀번호를 잊으셨나요?</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </main>
+
+        <script src="js/login.js"></script>
+      </body>
+    </>
+  );
 };
 
 export default KilLogin;
