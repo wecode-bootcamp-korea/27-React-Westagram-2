211ef1b99dc685fa5e596f90943db36101d023a1
JooyoungHwnag <71796608+JooyoungHwnag@users.noreply.github.com>
2021-11-20 16:31:16 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/hjy/Login/HwangLoginForm.js
+++ src/pages/hjy/Login/HwangLoginForm.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
-
-import './HwangLoginForm.scss';
 import { Link, useNavigate } from 'react-router-dom';
 
+import './HwangLoginForm.scss';
+
 const HwangLoginForm = () => {
-  const [loginId, setLoginId] = useState('');
-  const [pw, setPw] = useState('');
+  //이메일, 비밀번호 확인
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const handleIdInput = event => {
-    setLoginId(event.target.value);
+    setEmail(event.target.value);
   };
-  const handlePwInput = evnet => {
-    setPw(evnet.target.value);
+  const handlePasswordInput = event => {
+    setPassword(event.target.value);
   };
+  //유효성 검사
+  const [isActive, setIsActive] = useState(false);
+  const isPassedLogin = () => {
+    return email.includes('@') && password.length > 4
+      ? setIsActive(true)
+      : setIsActive(false);
+  };
+  //인증통과시 메인화면 이동
   const navigate = useNavigate();
   const goToMain = () => {
-    navigate('/main');
+    isActive === true ? navigate('/hwangmain') : navigate('hwanglogin');
   };
   return (
     <div className="boxWrap">
@@ -22,18 +31,26 @@ const HwangLoginForm = () => {
       <div className="loginWrap">
         <input
           onChange={handleIdInput}
+          onKeyUp={isPassedLogin}
           className="idInput"
           type="text"
           placeholder="전화번호, 사용자 이름 또는 이메일"
         />
         <input
-          onChange={handlePwInput}
-          className="passwordInput"
+          onChange={handlePasswordInput}
+          onKeyUp={isPassedLogin}
+          className="password"
           type="text"
           placeholder="비밀번호"
         />
-        <button className="btnDefault btnLightBlue">
-          <Link to="/hwangmain">로그인</Link>
+        <button
+          type="submit"
+          className={isActive ? 'activeBtn' : 'unactiveBtn'}
+          onClick={goToMain}
+          disabled={email === '' || password === '' ? true : false}
+          // disabled={!email || !password}
+        >
+          로그인
         </button>
       </div>
       <p className="searchPassword">
