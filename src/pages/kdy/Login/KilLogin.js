8e6766f842c6a5488d39e489d4cfc5f763c0a803
sanha <kgpaper@naver.com>
2021-11-24 16:42:27 +0900

    Add: 회원 가입 혹은 로그인기능 백엔드 접합 - fetch를 이용한 로그인 및 회원가입 예제 구현 - preventDefault를 이용한 form submit 방지

--- src/pages/kdy/Login/KilLogin.js
+++ src/pages/kdy/Login/KilLogin.js
@@ -4,10 +4,10 @@ import './KilLogin.scss';
 
 const KilLogin = () => {
   const [userInfo, setUserInfo] = useState({ userId: '', userPw: '' });
-  const [checkId, setCheckId] = useState({ value: false });
-  const [checkPw, setCheckPw] = useState({ value: false });
+  const [checkInput, setCheckInput] = useState(true);
 
-  const loginButton = useRef();
+  const idInput = useRef();
+  const pwInput = useRef();
 
   const handleUserInput = e => {
     const { name, value } = e.target;
@@ -16,30 +16,54 @@ const KilLogin = () => {
       [name]: value,
     });
 
-    idValid();
-    pwValid();
+    inputValid();
   };
 
-  const idValid = () => {
+  const onSignUp = async () => {
+    const data = await fetch('http://10.58.3.62:8000/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'wecode111',
+        email: 'wecode27th@naver.com',
+        password: 'Kk@950629',
+        phone_number: '01012345678',
+      }),
+    });
+    const res = await data.json();
+    await console.log('결과 : ', res);
+  };
+
+  const onSignIn = async e => {
+    e.preventDefault();
+    const { userId, userPw } = userInfo;
+    const data = await fetch('http://10.58.3.62:8000/users/signin', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: userId,
+        password: userPw,
+      }),
+    });
+    const res = await data.json();
+    await console.log('결과 : ', res);
+
+    setUserInfo({
+      userId: '',
+      userPw: '',
+    });
+
+    idInput.current.value = '';
+    pwInput.current.value = '';
+    setCheckInput(true);
+  };
+
+  const inputValid = () => {
     const regex = /[0-9a-zA-z-_.]*@[0-9a-zA-z-_.]/g;
 
-    if (userInfo.userId.match(regex) != null) {
-      setCheckId(true);
+    if (userInfo.userId.match(regex) != null && userInfo.userPw.length > 4) {
+      setCheckInput(false);
     } else {
-      setCheckId(false);
+      setCheckInput(true);
     }
-
-    loginButton.current.disabled = !(checkId && checkPw);
-  };
-
-  const pwValid = () => {
-    if (userInfo.userPw.length <= 4) {
-      setCheckPw(false);
-    } else {
-      setCheckPw(true);
-    }
-
-    loginButton.current.disabled = !(checkId && checkPw);
   };
 
   return (
@@ -55,6 +79,7 @@ const KilLogin = () => {
                   type="text"
                   placeholder="아이디"
                   id="id"
+                  ref={idInput}
                   onChange={handleUserInput}
                 />
               </div>
@@ -64,13 +89,21 @@ const KilLogin = () => {
                   type="password"
                   placeholder="비밀번호"
                   id="pw"
+                  ref={pwInput}
                   onChange={handleUserInput}
                 />
               </div>
               <div className="inputWrapper">
-                <button id="loginButton" disabled ref={loginButton}>
+                <button
+                  id="loginButton"
+                  disabled={checkInput}
+                  onClick={onSignIn}
+                >
                   로그인
                 </button>
+              </div>
+              <div className="inputWrapper pwRemind" onClick={onSignUp}>
+                <Link to="#">회원가입</Link>
               </div>
               <div className="inputWrapper pwRemind">
                 <Link to="#">비밀번호를 잊으셨나요?</Link>
